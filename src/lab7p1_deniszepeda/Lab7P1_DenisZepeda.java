8f0934bae2868a0980aa2149b2cbb885d66ae530
Koxtt <itzjosue06@gmail.com>
2026-06-05 15:52:04 -0600

    Third Commit

--- src/lab7p1_deniszepeda/Lab7P1_DenisZepeda.java
+++ src/lab7p1_deniszepeda/Lab7P1_DenisZepeda.java
@@ -19,7 +19,7 @@ public class Lab7P1_DenisZepeda {
     public static Scanner entry = new Scanner(System.in);
     public static Random r = new Random();
     public static Random aleatorio = new Random();
-    
+
     public static void main(String[] args) {
         int opc = 0;
         do {
@@ -30,55 +30,129 @@ public class Lab7P1_DenisZepeda {
             opc = entry.nextInt();
             switch (opc) {
                 case 1:
-                    YEPTDELR();
+                    panchoduro();
                     break;
                 case 2:
-                    multiplicacionmatrices()
+                    multiplicacionmatrices();
                     break;
                 case 3:
-
+                    System.out.println("Saliendo del codigo");
                     break;
                 default:
                     System.out.println("Error, Ingrese una opcion dentro del rango [1-2]");
-             
+
             }
-        } while (opc!=3);
+        } while (opc != 3);
+    }    
         
-        public static void YEPTDELR(){
-            int filas = 8;
-            int columnas = 7;
-            char[][] refri = new char [filas][columnas];
-            
-            int filacomida = 0;
-            while (filacomida<filas) {                
-                int columnascomida=0;
-                while (columnascomida<columnas) {                    
-                    refri[filacomida][columnascomida] = '*';
-                    columnascomida+=1;
-                }//fin del while de columnas
-                filacomida=filacomida+1;
-            }//fin del while de filas
-            int verdurapodrida=4;
-            int tortilla =3;
-            int sobras = 3;
-            int pancho=1;
-            
-            int malas = verdurapodrida+tortilla+sobras+pancho;
-            int score = 0;
-            
-            while (verdurapodrida>0) {                
-                int filarandy = aleatorio.nextInt();
-                int colarandy = aleatorio.nextInt();
-                if (refri[filarandy][colarandy] == '*') {
-                    
+
+    public static void panchoduro(){
+        int filas = 8;
+        int columnas = 7;
+        char[][] refri = new char[filas][columnas];
+
+        int filacomida = 0;
+        while (filacomida < filas) {
+            int columnascomida = 0;
+            while (columnascomida < columnas) {
+                refri[filacomida][columnascomida] = '*';
+                columnascomida += 1;
+            }//fin del while de columnas
+            filacomida = filacomida + 1;
+        }//fin del while de filas
+        
+        //contadores de comida 
+        int verdurapodrida = 4; 
+        int tortilla = 3;
+        int sobras = 3;
+        int pancho = 1;
+
+        int malas = verdurapodrida + tortilla + sobras + pancho;
+        int score = 0;
+
+        while (verdurapodrida > 0) {
+            int filarandy = aleatorio.nextInt();
+            int colarandy = aleatorio.nextInt();
+            if (refri[filarandy][colarandy] == '*') {
+                refri[filarandy][colarandy] = 'V';
+                verdurapodrida = verdurapodrida - 1;
+            }//fin del if
+        }//fin del while
+
+        while (tortilla > 0) {
+            int filarandy = aleatorio.nextInt();
+            int colarandy = aleatorio.nextInt();
+            if (refri[filarandy][colarandy] == '*') {
+                refri[filarandy][colarandy] = 'T';
+                tortilla = tortilla - 1;
+            }//fin del if
+        }//fin del while
+
+        while (sobras > 0) {
+            int filarandy = aleatorio.nextInt();
+            int colarandy = aleatorio.nextInt();
+            if (refri[filarandy][colarandy] == '*') {
+                refri[filarandy][colarandy] = 'S';
+                sobras = sobras - 1;
+            }//fin del if
+        }//fin del while
+
+        while (pancho > 0) {
+            int filarandy = aleatorio.nextInt();
+            int colarandy = aleatorio.nextInt();
+            if (refri[filarandy][colarandy] == '*') {
+                refri[filarandy][colarandy] = 'P';
+                pancho = pancho - 1;
+            }//fin del if
+        }//fin del while
+        
+        while (malas>0) {            
+            System.out.println("YEPTDELR: ¿Y Ese Pancho Todo Duro en la Refri?");
+            int i = 0;
+            while (i<filas) {                
+               int j = 0;
+                while (j<columnas) {                    
+                    System.out.println("[" + refri[i][j] + "]");
+                    j+=1;
+                }//while j
+                System.out.println();
+                i+=1;
+            }//while i
+            System.out.println("Ingrese la Fila [0-7])");
+            int llenarFila = entry.nextInt();
+            System.out.println("Ingrese la Columna [0-8]");
+            int llenarCol = entry.nextInt();
+            if (llenarFila>= 0 && llenarFila<filas && llenarCol>= 0 && llenarCol<columnas) {
+                char casilla = refri[llenarFila][llenarCol];
+                if (casilla=='*') {
+                    System.out.println("Contiene comida buena");
+                } else if (casilla==' ') {
+                    System.out.println("Este espacio ya esta vacio");
+                } else if (casilla=='V') {
+                    score=score+1;
+                    System.out.println("Comida Purgada");
+                }else if (casilla=='T') {
+                    score=score+2;
+                    System.out.println("Comida Purgada");
+                }else if (casilla=='S') {
+                    score=score+3;
+                    System.out.println("Comida Purgada");
+                }else if (casilla=='P') {
+                    score=score+5;
+                    System.out.println("Comida Purgada");
                 }
+                refri[llenarFila][llenarCol] = ' ';
+                malas=malas-1;
+                System.out.println("Puntaje Total: " +score);
             }
             
-        }
+        }//fin del while de malas
+        
+        }//fin del metodo 1
         
         public static void multiplicacionmatrices(){
             
         }
     }
 
-}
+
